5dfcef994545dba6eb09f918e62a2c4b760f9d45
Oliver Cvetkovski <cvetkovski98@gmail.com>
2020-05-14 22:32:47 +0200

    feat: extract matrix init function

--- src/main/java/pp/finki/ukim/mk/annocuda/services/TestService.java
+++ src/main/java/pp/finki/ukim/mk/annocuda/services/TestService.java
@@ -2,8 +2,10 @@ package pp.finki.ukim.mk.annocuda.services;
 
 public interface TestService {
     Object cpuVectorSum(double[] vector1, double[] vector2);
+
     Object cpuMatrixMultiplication(double[][] vector1, double[][] vector2);
 
     Object gpuVectorSum(double[] vector1, double[] vector2);
+
     Object gpuMatrixMultiplication(double[][] vector1, double[][] vector2);
 }
--- src/main/java/pp/finki/ukim/mk/annocuda/web/EntryPointController.java
+++ src/main/java/pp/finki/ukim/mk/annocuda/web/EntryPointController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import pp.finki.ukim.mk.annocuda.annotations.GPUServiceProvider;
 import pp.finki.ukim.mk.annocuda.services.TestService;
 
-import java.io.IOException;
 import java.util.Random;
 
 @RestController
@@ -18,19 +17,20 @@ public class EntryPointController {
     private final TestService testService;
     double[] arr1 = null;
     double[] arr2 = null;
+    double[][] mat1 = null;
+    double[][] mat2 = null;
 
     public EntryPointController(GPUServiceProvider gpuServiceProvider, TestService testService) {
         this.testService = gpuServiceProvider.bindWithGpuAnnotationProcessor(testService, TestService.class);
     }
 
     @GetMapping("/vectorAddCpu")
-    public ResponseEntity<Object[]> vectorAddCpu(@RequestParam long arraySize) throws IOException {
+    public ResponseEntity<Object[]> vectorAddCpu(@RequestParam long arraySize) {
         generateRandomArrays(arraySize);
         long t1 = System.currentTimeMillis();
         Object p = testService.cpuVectorSum(arr1, arr2);
-        long t2 = System.currentTimeMillis();
-        long time = t2 - t1;
-        return ResponseEntity.ok(new Object[]{p, time});
+        long time = System.currentTimeMillis() - t1;
+        return ResponseEntity.ok(new Object[]{time, p});
     }
 
     @GetMapping("/vectorAddGpu")
@@ -38,45 +38,26 @@ public class EntryPointController {
         generateRandomArrays(arraySize);
         long t1 = System.currentTimeMillis();
         Object p = testService.gpuVectorSum(arr1, arr2);
-        long t2 = System.currentTimeMillis();
-        long time = t2 - t1;
-        return ResponseEntity.ok(new Object[]{p, time});
+        long time = System.currentTimeMillis() - t1;
+        return ResponseEntity.ok(new Object[]{time, p});
     }
 
     @GetMapping("/matMulGpu")
-    public ResponseEntity<Object[]> matrixGpu() {
+    public ResponseEntity<Object[]> matrixGpu(@RequestParam long arraySize) {
+        generateRandomMatrices(arraySize);
         long t1 = System.currentTimeMillis();
-        int sz = 32;
-        double[][] test1 = new double[sz][sz];
-        double[][] test2 = new double[sz][sz];
-        for (int i = 0; i < sz; i++) {
-            for (int j = 0; j < sz; j++) {
-                test1[i][j] = 1;
-                test2[i][j] = 1;
-            }
-        }
-        Object p = testService.gpuMatrixMultiplication(test1, test2);
-        long t2 = System.currentTimeMillis();
-        long time = t2 - t1;
-        return ResponseEntity.ok(new Object[]{p, time});
+        Object p = testService.gpuMatrixMultiplication(mat1, mat2);
+        long time = System.currentTimeMillis() - t1;
+        return ResponseEntity.ok(new Object[]{time});
     }
 
     @GetMapping("/matMulCpu")
-    public ResponseEntity<Object[]> matrixCpu() {
+    public ResponseEntity<Object[]> matrixCpu(@RequestParam long arraySize) {
+        generateRandomMatrices(arraySize);
         long t1 = System.currentTimeMillis();
-        int sz = 32;
-        double[][] test1 = new double[sz][sz];
-        double[][] test2 = new double[sz][sz];
-        for (int i = 0; i < sz; i++) {
-            for (int j = 0; j < sz; j++) {
-                test1[i][j] = 1;
-                test2[i][j] = 1;
-            }
-        }
-        Object p = testService.cpuMatrixMultiplication(test1, test2);
-        long t2 = System.currentTimeMillis();
-        long time = t2 - t1;
-        return ResponseEntity.ok(new Object[]{p, time});
+        Object p = testService.cpuMatrixMultiplication(mat1, mat2);
+        long time = System.currentTimeMillis() - t1;
+        return ResponseEntity.ok(new Object[]{time});
     }
 
     private void generateRandomArrays(long arraySize) {
@@ -85,11 +66,30 @@ public class EntryPointController {
         }
         int rangeMin = 1;
         int rangeMax = 10;
-        arr1 = arr2 = new double[Math.toIntExact(arraySize)];
+        arr1 = new double[Math.toIntExact(arraySize)];
+        arr2 = new double[Math.toIntExact(arraySize)];
         for (int i = 0; i < arraySize; i++) {
             Random r = new Random();
             double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
             arr1[i] = arr2[i] = randomValue;
+        }
+    }
+
+    private void generateRandomMatrices(long arraySize) {
+        if (mat1 != null && mat2 != null && arraySize == mat1.length) {
+            return;
+        }
+        int rangeMin = 1;
+        int rangeMax = 10;
+        mat1 = new double[Math.toIntExact(arraySize)][Math.toIntExact(arraySize)];
+        mat2 = new double[Math.toIntExact(arraySize)][Math.toIntExact(arraySize)];
+
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = 0; j < arraySize; j++) {
+                Random r = new Random();
+                double randomValue = rangeMin + (rangeMax - rangeMin) * r.nextDouble();
+                mat1[i][j] = mat2[i][j] = randomValue;
+            }
         }
     }
 }
